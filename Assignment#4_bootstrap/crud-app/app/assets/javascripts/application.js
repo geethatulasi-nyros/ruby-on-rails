864b465ae160e37a50ba75d39f62c2298142c782
geethatulasi-nyros <geethatulasi.nyros@gmail.com>
2019-03-28 18:56:12 +0530

    rails assignment#4 bootstrap

---
+++ Assignment#4_bootstrap/crud-app/app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require popper
+//= require turbolinks
+//= require bootstrap
+//= require_tree.
+//=require jquery_ujs
+//=require bootbox
+ 
---
+++ Assignment#4_bootstrap/crud-app/app/views/articles/articles.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('#link').click(function() {
+    bootbox.alert("Hello world!");  
+  })
+})
